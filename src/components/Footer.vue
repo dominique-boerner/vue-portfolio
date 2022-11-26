8e9343ed0efe80ed1bbb07494074ab66abb47dca
Dominique Börner <dominique@mukosoft.de>
2022-11-26 13:21:11 +0100

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="flex justify-between bg-primary text-white h-32 p-4">
+  <div class="flex justify-between bg-primary text-white p-4">
     <div class="flex flex-col">
       <span>Dominique Börner</span>
       <span
         >E-Mail:
         <a class="underline" href="mailto:dominiqueboerner@outlook.de"
           >dominiqueboerner@outlook.de</a
+        ></span
+      >
+      <span
+        >GitHub:
+        <a
+          class="underline"
+          href="http://github.dominiqueboerner.me/"
+          target="_blank"
+          >github.dominiqueboerner.me</a
+        ></span
+      >
+      <span
+        >XING:
+        <a
+          class="underline"
+          href="http://xing.dominiqueboerner.me"
+          target="_blank"
+          >xing.dominiqueboerner.me</a
         ></span
       >
     </div>
