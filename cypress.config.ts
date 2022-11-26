1d9a84506d29b4bf21ff139229318318b9391b09
Dominique Börner <dominique@mukosoft.de>
2022-11-26 13:46:33 +0100

    remove todo

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    video: false,
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
     baseUrl: "http://127.0.0.1:4173",
   },
